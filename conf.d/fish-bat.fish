de8d76b85fffebac259f85bfd2d17dcb16bb98ca
givensuman <givensuman@duck.com>
2025-01-03 12:02:37 -0500

    Better utilize fisher

---
+++ conf.d/fish-bat.fish
@@ -0,0 +1,37 @@
+function _fish_bat_install --on-event fish_bat_install
+    # For Ubuntu and Debian-based `bat` packages
+    # the `bat` program is named `batcat`
+    if command -q batcat
+        alias rcat="$(which cat)"
+        alias cat="$(which batcat)"
+
+        # Set manpager to use `bat`
+        set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ batcat\ -l\ map\ -p\'
+        set -gx MANROFFOPT -c
+
+    else if command -q bat # For all other systems
+        alias rcat="$(which cat)"
+        alias cat="$(which bat)"
+
+        # Set manpager to use `bat`
+        set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ bat\ -l\ map\ -p\'
+        set -gx MANROFFOPT -c
+
+    else
+        echo "bat is not installed but you're"
+        echo "sourcing the fish plugin for it"
+    end
+end
+
+function _fish_bat_uninstall --on-event fish_bat_uninstall
+    functions --erase rcat
+    functions --erase cat
+
+    set --erase MANPAGER
+    set --erase MANROFFOPT
+end
+
+function _fish_bat_update --on-event fish_bat_update
+    _fish_bat_uninstall
+    _fish_bat_install
+end
