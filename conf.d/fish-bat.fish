fa9e32d2ad1ab180313fdf8c7472a01e79937d26
givensuman <givensuman@duck.com>
2025-09-18 09:34:33 -0400

    Update MANPAGER logic, add help message alias, consolidate logic

--- conf.d/fish-bat.fish
+++ conf.d/fish-bat.fish
@@ -1,30 +1,29 @@
 function _fish_bat_install --on-event fish-bat_install
-    # For Ubuntu and Debian-based `bat` packages
-    # the `bat` program is named `batcat`
+    # For Ubuntu/Debian systems where `bat` is installed as `batcat`
     if command -q batcat
-        alias rcat="$(which cat)"
-        alias cat="$(which batcat)"
-
-        # Set manpager to use `bat`
-        set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ batcat\ -l\ man\ -p\'
-        set -gx MANROFFOPT -c
-
-        batcat cache --build
-    else if command -q bat # For all other systems
-        alias rcat="$(which cat)"
-        alias cat="$(which bat)"
-
-        # Set manpager to use `bat`
-        set -gx MANPAGER sh\ -c\ \'col\ -bx\ \|\ bat\ -l\ man\ -p\'
-        set -gx MANROFFOPT -c
-
-        bat cache --build
+        set bat_cmd batcat
+    else if command -q bat
+        set bat_cmd bat
     else # `bat` command not found
         echo "bat is not installed but you're"
         echo "sourcing the fish plugin for it"
 
         return 1
     end
+
+    # Replace `cat` with `bat`
+    alias --save rcat="$(which cat)"
+    alias --save cat="$(which $bat_cmd)"
+
+    # Set manpager to use `bat`
+    set -gx MANPAGER sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | '$bat_cmd' -p -lman'
+    man 2 select
+
+    # Colorize help messages
+    abbr -a --position anywhere -- --help '--help | '$bat_cmd' -plhelp'
+    abbr -a --position anywhere -- -h '-h | '$bat_cmd' -plhelp'
+
+    $bat_cmd cache --build
 end
 
 function _fish_bat_uninstall --on-event fish-bat_uninstall
